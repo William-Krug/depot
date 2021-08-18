6764d63b38f1dae114ffe593ee1e66ae901d0760
William Krug <william.p.krug@gmail.com>
2021-08-18 16:44:09 -0500

    unit tests for `products_controller`

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  # Fields must be present
+  validates :title, :description, :image_url, presence: true
+
+  # Title must be unique
+  validates :title, uniqueness: true
+
+  # URL must end in .gif, .jpg, or .png
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
+  # Price must be at least $0.01
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
