7eeb5a260674fae17f3dea24b56cc0cf1b901eb5
William Krug <william.p.krug@gmail.com>
2021-08-20 15:47:10 -0500

    added functional testing to `products_controller`

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main table tr', 4
+    assert_select 'h1', 'Products'
   end
 
   test "should get new" do
