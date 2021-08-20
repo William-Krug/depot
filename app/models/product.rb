959d3cb4cb6d8cba237c310ed1d87c5aeeaaee2c
William Krug <william.p.krug@gmail.com>
2021-08-20 16:14:02 -0500

    added cart and line item scaffolding

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
   # Fields must be present
   validates :title, :description, :image_url, presence: true
 
@@ -14,4 +17,14 @@ class Product < ApplicationRecord
 
   # Price must be at least $0.01
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
