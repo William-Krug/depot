42349f0c5f5c9bfa09ea5533ae8d8f427c64a4d8
William Krug <william.p.krug@gmail.com>
2021-08-27 13:49:23 -0500

    moved shopping cart into side nav

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,11 @@
 class StoreController < ApplicationController
   include StoreIndexCounter
+  include CurrentCart
 
   before_action :update_index_count, only: [:index]
+  before_action :set_cart
 
   def index
     @products = Product.order(:title)
-    @current_time = Time.now.strftime("%-d %b, %Y %l:%-M:%-S")
   end
 end
