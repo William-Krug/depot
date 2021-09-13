efdb7b93b4ab8ae0257d223b191bb287f52f7c31
William Krug <william.p.krug@gmail.com>
2021-09-13 16:59:24 -0500

    added styling to "remove" button

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,11 +59,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    print('*** line_items_controller#destroy ***')
-
-    @line_item.destroy if @line_item
+    @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      # format.html { redirect_to store_index_url, notice: "Line item has been removed." }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
