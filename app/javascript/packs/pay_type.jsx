0fc23ec1de450dedadc89e2377afe4db2ab42924
William Krug <william.p.krug@gmail.com>
2021-11-22 15:25:53 -0600

    test commit

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import PayTypeSelector from 'PayTypeSelector';
+// import PayTypeSelector from './PayTypeSelector';
+// import PayTypeSelector from '../PayTypeSelector';
+// import PayTypeSelector from '../PayTypeSelector/index';
 
 document.addEventListener('turbolinks:load', function () {
   var element = document.getElementById('pay-type-component');
