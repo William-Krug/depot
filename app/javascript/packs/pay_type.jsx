f4488dcbe49ed83179df703048e309f6429fe445
William Krug <william.p.krug@gmail.com>
2021-11-22 16:29:52 -0600

    added "xxxPayType.jsx" components

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import PayTypeSelector from 'PayTypeSelector';
-// import PayTypeSelector from './PayTypeSelector';
-// import PayTypeSelector from '../PayTypeSelector';
-// import PayTypeSelector from '../PayTypeSelector/index';
 
 document.addEventListener('turbolinks:load', function () {
   var element = document.getElementById('pay-type-component');
